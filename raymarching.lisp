5f96072742b7cc555a26568eaefa3765f01c836e
azimut <azimut.github@protonmail.com>
2019-02-15 00:06:34 -0300

    raymarching: init, quad loads fine here

---
+++ raymarching.lisp
@@ -0,0 +1,163 @@
+(in-package #:incandescent)
+
+;; Raymarching as postprocess, to integrate it with a normal
+;; rasterized scene.
+
+;; INPUTS:
+;; - *dimensions*
+;; - *samd*
+;; - *sam*
+
+
+;; VARJO (v:-) should negate the single vector
+;;       up/down/right/left unity like for (v! 0 1 0) et al
+;;       setf for m4:get-row // meh!
+;;       for() support global vars
+(defvar *quad-3d* NIL "BUFFER-STREAM that has the geometry for the quad")
+(defvar *frustum* NIL "C-ARRAY that has the frustum corners???")
+
+(defvar *ray-fbo* NIL)
+(defvar *ray-sam* NIL)
+
+(defvar *max-steps* 10f0)
+(defvar *min-distance* .001)
+(defvar *max-distance* 1000f0)
+
+
+(defun init-raymarch ()
+  (when *ray-fbo* (free *ray-fbo*))
+  (setf *ray-fbo* (make-fbo `(0 :dimensions ,*dimensions*
+                                :element-type :rgba16f))
+        *ray-sam* (sample (attachment-tex *ray-fbo* 0)
+                          :wrap :clamp-to-edge))
+  (unless *frustum*
+    (setf *frustum*
+          (make-c-array (get-frustum-corners *currentcamera*)
+                        :element-type :vec3 :dimensions 4)))
+  (unless *quad-3d*
+    ;; Half screen billboard
+    ;; (setf *quad-3d*
+    ;;       (let* ((verts (make-gpu-array (list (v! -.5 -.5 3)
+    ;;                                           (v!  .5 -.5 2)
+    ;;                                           (v! -.5  .5 1)
+    ;;                                           (v!  .5  .5 0))))
+    ;;              (buf (make-buffer-stream verts :primitive :triangle-strip)))
+    ;;         buf))
+    ;; 1 0 0 // BL
+    ;; 0 1 0 // TR
+    ;; 0 0 1 // BR
+    ;; 0 0 0 // TL
+    (setf *quad-3d*
+          (let* ((verts (make-gpu-array `((,(v! -1 -1 3) ,(v! 1 0 0) ,(v! 0 0))
+                                          (,(v!  1  1 1) ,(v! 0 1 0) ,(v! 1 1))
+                                          (,(v!  1 -1 2) ,(v! 0 0 1) ,(v! 1 0))
+                                          (,(v! -1  1 0) ,(v! 0 0 0) ,(v! 0 1)))
+                                        :dimensions 4 :element-type 'g-pnt))
+                 (indi  (make-gpu-array '(0 1 2 1 0 3)
+                                        :dimensions 6
+                                        :element-type :unsigned-short))
+                 (buf   (make-buffer-stream verts
+                                            :index-array indi
+                                            :primitive :triangle-strip)))
+            buf))
+    ))
+
+(defvar *tmp2* (make-instance 'actor))
+(defun draw-raymarching ()
+  ;; (push-g (get-frustum-corners *currentcamera*)
+  ;;         *frustum*)
+  (with-fbo-bound (*ray-fbo*)
+    (clear *ray-fbo*)
+    (map-g #'raymarch-pipe *quad-3d*
+           :samd *samd*
+           :sam  *sam*
+           :cam-pos (pos *currentcamera*)
+           :frustum-corners *frustum*
+           :world-view (world->view *currentcamera*)
+           :view-world (m4:inverse (model->world *currentcamera*)))))
+
+
+;;--------------------------------------------------
+;; SPHERE
+
+(defun-g distance-estimator ((p :vec3) (c :vec3) (r :float))
+  (- (length (- p c)) r))
+
+(defun-g calc-normal ((pos :vec3))
+  (let* ((eps (v! .001 0))
+         (nor (v! (- (distance-estimator (+ pos (s~ eps :xyy)) (v! 0 0 -30) 2f0)
+                     (distance-estimator (- pos (s~ eps :xyy)) (v! 0 0 -30) 2f0))
+                  (- (distance-estimator (+ pos (s~ eps :yxy)) (v! 0 0 -30) 2f0)
+                     (distance-estimator (- pos (s~ eps :yxy)) (v! 0 0 -30) 2f0))
+                  (- (distance-estimator (+ pos (s~ eps :yyx)) (v! 0 0 -30) 2f0)
+                     (distance-estimator (- pos (s~ eps :yyx)) (v! 0 0 -30) 2f0)))))
+    (normalize nor)))
+
+(defun-g raymarch ((from :vec3) (direction :vec3) (s :float))
+  (let ((ret (v4! 0))
+        (max-steps 20)
+        (draw-distance 40f0)
+        (tt 0f0))
+    (for (steps 0) (< steps max-steps) (++ steps)
+         (when (or (>= tt s) (> tt draw-distance))
+           (setf ret (v! 0 0 0 0))
+           (break))
+         (let* ((p (+ from (* direction tt)))
+                (d (distance-estimator p (v! 0 0 -10) 1f0)))
+           (when (< d .001)
+             (let* ((n (calc-normal p))
+                    (light (dot (v! 0 1 0) n)))
+               (setf ret (v4! (* light 1)))
+               (setf ret (v! 0 1 0 0))
+               (break)))
+           (incf tt d)))
+    ret))
+
+;;--------------------------------------------------
+;; PIPELINE
+;;
+
+;; https://github.com/Flafla2/Generic-Raymarch-Unity/blob/master/Assets/RaymarchGeneric.shader
+(defun-g raymarch-vert ((vert g-pnt) ;;(pos :vec3)
+                        &uniform
+                        (world-view :mat4)
+                        (view-world :mat4)
+                        (frustum-corners (:vec3 4)))
+  (let* ((pos   (pos vert))
+         (index (z pos))
+         (pos   (v! (x pos) (y pos) .1))
+         (view-pos (* world-view (v! pos 1)))
+         ;; get the eyespace view ray (normalized)
+         (ray (s~ (aref frustum-corners (int index))
+                  :xyz))
+         ;; Dividing by z "normalizes" it in the z axis
+	 ;; Therefore multiplying the ray by some number i gives the viewspace position
+	 ;; of the point on the ray with [viewspace z]=i
+         (ray (/ ray (abs (z ray))))
+         ;; Transform the ray from eyespace to worldspace
+         (ray (s~ (* view-world (v! ray 1)) :xyz))
+         ;;(ray (v! 1 1 1))
+         )
+    (values view-pos
+            (tex vert)
+            ;;(+ (s~ pos :xy) (v! .5 .5))
+            ray)))
+
+(defun-g raymarch-frag ((uv :vec2)
+                        (ray :vec3)
+                        &uniform
+                        (sam :sampler-2d)
+                        (samd :sampler-2d)
+                        (cam-pos :vec3))
+  (let* ((rd (normalize ray))
+         (ro cam-pos)
+         (depth (linear-eye-depth (x (texture samd uv))))
+         (depth (* depth (length ray)))
+         (color (s~ (texture sam uv) :xyz))
+         (add   (raymarch ro rd depth)))
+    add))
+
+(defpipeline-g raymarch-pipe (:triangle-strip)
+  :vertex   (raymarch-vert g-pnt;;:vec3
+                           )
+  :fragment (raymarch-frag :vec2 :vec3))
