e5fd6fd144f814db09f5f7ae349daf11ed7c70fa
azimut <azimut.github@protonmail.com>
2019-06-06 03:33:51 -0300

    add v3 quad

--- incandescent.lisp
+++ incandescent.lisp
@@ -9,6 +9,25 @@
 (defvar *vec3-forward* (v!  0  0  1))
 (defvar *vec3-back*    (v!  0  0 -1))
 
+(defvar *quad-stream-v3-data*
+  (list (v! -1.0   1.0 0)
+        (v! -1.0  -1.0 0)
+        (v!  1.0  -1.0 0)
+        (v! -1.0   1.0 0)
+        (v!  1.0  -1.0 0)
+        (v!  1.0   1.0 0)))
+
+(let ((stream nil))
+  (defun get-quad-stream-v3 ()
+    (declare (optimize (speed 3) (safety 1) (debug 1)))
+    (unless stream
+      (setf stream (make-buffer-stream
+                    (make-gpu-array *quad-stream-v3-data*
+                                    :element-type :vec3
+                                    :dimensions 6)
+                    :retain-arrays t)))
+    stream))
+
 (defun fmod (x y)
   (- x (* y (floor (/ x y)))))
 
