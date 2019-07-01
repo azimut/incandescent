4e91d4790c148fce2976fea6738a483c9689f14c
azimut <azimut.github@protonmail.com>
2019-07-01 05:01:33 -0300

    ode: setup each contact, return-from block

--- ode/ode.lisp
+++ ode/ode.lisp
@@ -34,47 +34,50 @@
         *space* nil
         *contactgroup* nil))
 
-(progn (claw:defcallback near-callback :void ((data :pointer)
-                                              (o1 %ode:geom-id)
-                                              (o2 %ode:geom-id))
-         (declare (ignore data))
-         (claw:c-with ((contact %ode:contact :calloc t :count 10))
-           (let* ((b1 (%ode:geom-get-body o1))
-                  (b2 (%ode:geom-get-body o2))
-                  (sj (%ode:are-connected-excluding b1 b2 %ode:+joint-type-contact+)))
-             (when (not (and b1 b2 (plusp sj)))
-               (setf (contact :surface :mode) (logior %ode:+contact-bounce+
-                                                      ;; %ode:+contact-slip1+
-                                                      ;; %ode:+contact-slip2+
-                                                      ;; %ode:+contact-approx1+
-                                                      ;; %ode:+contact-soft-erp+
-                                                      %ode:+contact-soft-cfm+)
-                     ;; (contact :surface :slip1) .7d0
-                     ;; (contact :surface :slip2) .7d0
-                     ;;(contact :surface :soft-erp) .96d0
-                     ;; friction parameter
-                     (contact :surface :mu) ode:+infinity+
-                     (contact :surface :mu2) 0d0
-                     ;; bounce is the amount of "bouncyness"
-                     (contact :surface :bounce) .1d0
-                     ;; bounce_vel is the minimum incoming velocity to cause a bounce
-                     (contact :surface :bounce-vel) .1d0
-                     ;; constraint force mixing parameter
-                     (contact :surface :soft-cfm) .01d0)
-               (let ((numc (%ode:collide o1 o2 10 (contact :geom &) (claw:sizeof '%ode:contact))))
-                 (when (> numc 0)
-                   (dotimes (i numc)
-                     (let ((c (%ode:joint-create-contact *world*
-                                                         *contactgroup*
-                                                         (contact i))))
-                       (%ode:joint-attach c b1 b2)))))))))
-       (let ((stepper (make-stepper (seconds .01) (seconds .01))))
-         (defun ode-update ()
-           "updates the objets within the physics engine"
-           (when (and *world* (funcall stepper))
-             (%ode:space-collide *space* nil (claw:callback 'near-callback))
-             (%ode:world-quick-step *world* 0.004d0)
-             (%ode:joint-group-empty *contactgroup*)))))
+(progn
+  (block nn
+    (claw:defcallback near-callback :void ((data :pointer)
+                                           (o1 %ode:geom-id)
+                                           (o2 %ode:geom-id))
+      (declare (ignore data))
+      (let ((b1 (%ode:geom-get-body o1))
+            (b2 (%ode:geom-get-body o2)))
+        (when (and b1 b2 (plusp (%ode:are-connected-excluding b1 b2 %ode:+joint-type-contact+)))
+          (return-from nn))
+        (claw:c-with ((contact %ode:contact :calloc t :count 6))
+          (dotimes (i 5)
+            (setf (contact i :surface :mode) (logior %ode:+contact-bounce+
+                                                     ;; %ode:+contact-slip1+
+                                                     ;; %ode:+contact-slip2+
+                                                     ;; %ode:+contact-approx1+
+                                                     ;; %ode:+contact-soft-erp+
+                                                     %ode:+contact-soft-cfm+)
+                  ;; (contact i :surface :slip1) .7d0
+                  ;; (contact i :surface :slip2) .7d0
+                  ;;(contact i :surface :soft-erp) .96d0
+                  ;; friction parameter
+                  (contact i :surface :mu) ode:+infinity+
+                  (contact i :surface :mu2) 0d0
+                  ;; bounce is the amount of "bouncyness"
+                  (contact i :surface :bounce) .1d0
+                  ;; bounce_vel is the minimum incoming velocity to cause a bounce
+                  (contact i :surface :bounce-vel) .1d0
+                  ;; constraint force mixing parameter
+                  (contact i :surface :soft-cfm) .01d0))
+          (dotimes (i (%ode:collide o1 o2 6
+                                    (contact :geom &)
+                                    (claw:sizeof '%ode:contact)))
+            (%ode:joint-attach (%ode:joint-create-contact *world*
+                                                          *contactgroup*
+                                                          (contact i))
+                               b1 b2))))))
+  (let ((stepper (make-stepper (seconds .01) (seconds .01))))
+    (defun ode-update ()
+      "updates the objets within the physics engine"
+      (when (and *world* (funcall stepper))
+        (%ode:space-collide *space* nil (claw:callback 'near-callback))
+        (%ode:world-quick-step *world* 0.005d0)
+        (%ode:joint-group-empty *contactgroup*)))))
 
 ;; FIME: leaking? c-with would free it...
 (defun ode-geom-get-position (geom)
