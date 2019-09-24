2d9cd32eb809599eea86c14108dfec66c618b955
azimut <azimut.github@protonmail.com>
2019-09-24 09:51:07 -0300

    camera: animation--, shortcuts conversions, frustumrays?

--- camera.lisp
+++ camera.lisp
@@ -8,7 +8,7 @@
    (far    :initarg :far   :accessor far)
    (shots  :initarg :shots :accessor shots
            :documentation "vector of shots, which are vectors of frames")
-   (n-shot :initarg :n-shot :documentation "number of shots")
+   (n-shot :initarg :n-shot :documentation "number of shots, or nil if disabled")
    (atime  :initarg :atime  :documentation "current animation time")
    (aspeed :initarg :aspeed :documentation "animation speed")
    (frame-size :initarg :frame-size
@@ -32,6 +32,9 @@
    :fov 60f0))
 
 (defparameter *camera*
+  (make-instance 'pers :far 200f0))
+
+(defparameter *camera2*
   (make-instance 'pers :far 1000f0))
 
 (defparameter *camera1*
@@ -52,7 +55,9 @@
   (declare (type rtg-math.types:vec3)
            (type camera camera))
   (setf (pos camera) pos)
-  (setf (rot camera) (q:identity)))
+  (setf (rot camera) (q:identity))
+  ;;(shot-clear camera)
+  )
 
 (defun next-camera ()
   "rotates current value on *CURRRENTCAMERA*
@@ -88,6 +93,12 @@
   (m4:* (projection camera)
         (world->view camera)))
 
+(defun model->view (a c)
+  (m4:* (world->view c) (model->world a)))
+
+(defun model->clip (a c)
+  (m4:* (world->clip c) (model->world a)))
+
 ;; Used for Raymarching. But I think it should be useful elsewhere.
 ;; https://github.com/Flafla2/Generic-Raymarch-Unity/blob/master/Assets/RaymarchGeneric.cs
 (defmethod get-frustum-corners ((camera pers))
@@ -116,6 +127,25 @@
                       (v! bottom-right)
                       (v! bottom-left)))))
 
+;; SetFrustumRays()
+(defun get-frustum-rays-v3 ()
+  (let ((camera *currentcamera*))
+    (loop :for uv :in (list (v! 0 0) (v! 1 0) (v! 1 1) (v! 0 1))
+          :collect (v3:- (m4:*v3 (m4:inverse (world->view camera))
+                                 (v! uv (far camera)))
+                         (pos camera)))))
+
+(defun get-frustum-rays-v4 ()
+  (let ((camera *currentcamera*))
+    (loop :for uv :in (list (v! 0 0)
+                            (v! 1 0)
+                            (v! 1 1)
+                            (v! 0 1))
+          :collect (v! (v3:- (m4:*v3 (m4:inverse (world->view camera))
+                                     (v! uv (far camera)))
+                             (pos camera))
+                       0f0))))
+
 (defmethod camera-to-camera ((src-camera camera) (dst-camera camera))
   "copies rotation and position from SRC-CAMERA to DST-CAMERA"
   (setf (pos dst-camera) (copy-seq (pos src-camera)))
@@ -124,121 +154,23 @@
 
 (let ((old-pos (v3! 0))
       (old-rot (v3! 0)))
-  (defun switcharoo (&optional (camera *currentcamera*))
-    (let ((current-pos (copy-seq (pos camera)))
-          (current-rot (copy-seq (rot camera))))
-      (if (v3:0p old-pos)
-          (setf old-pos current-pos
-                old-rot current-rot
-                (pos camera) (v3! 0))
-          (setf (pos camera) old-pos
-                (rot camera) old-rot
-                old-pos (v3! 0))))))
-
-;;--------------------------------------------------
-;; ANIMATION
-;;--------------------------------------------------
-
-(defun shot-on (&optional (camera *currentcamera*))
-  (with-slots (n-shot atime) camera
-    (setf atime 0f0)
-    (setf n-shot 0)))
-
-(defun shot-toggle (&optional (camera *currentcamera*))
-  "enable/disable animation play"
-  (with-slots (n-shot atime) camera
-    (setf atime 0f0)
-    (if (numberp n-shot)
-        (setf n-shot NIL)
-        (setf n-shot 0))))
-
-(defun shot-status (&optional (camera *currentcamera*))
-  (with-slots (shots n-shot) camera
-    (format T "Shot status: ~a~%" n-shot)
-    (format T "Shots: ~a~%" shots)))
-
-(defun shot-pop ()
-  (with-slots (shots) *currentcamera*
-    (vector-pop (aref shots 0))))
-
-(defun shot-add (time &optional (camera *currentcamera*) (shot 0))
-  "use this to add the current POS/ROT frame into the SHOT"
-  (declare (type unsigned-byte shot))
-  (with-slots (pos rot shots) camera
-    (let ((element (list (copy-seq pos) (copy-seq rot) time)))
-      (vector-push-extend element
-                          (aref shots shot)))))
-
-(defun shot-show (frame &optional (camera *currentcamera*))
-  "REPL helper to switch camera to frames in shot"
-  (when-let* ((shot         (aref (shots camera) 0))
-              (has-shots    (not (emptyp shot)))
-              (actual-frame (mod frame (fill-pointer shot)))
-              (element      (aref shot actual-frame))
-              (position     (first element))
-              (rotation     (second element)))
-    (with-slots (pos rot) camera
-      (setf pos position)
-      (setf rot rotation))
-    frame))
-
-(defun shot-clear (&optional (camera *currentcamera*))
-  "REMOVE all shots"
-  (with-slots (shots n-shot) camera
-    (setf n-shot NIL)
-    (setf shots (vect (vect)))))
-
-(defmethod animate ((camera camera))
-  "actually animate the thing
-   FIXME: DOMO ARIGATO"
-  (when-let* ((n-shot   (slot-value camera 'n-shot))
-              (shot     (aref (shots camera) n-shot))
-              (n-frames (fill-pointer shot))
-              (enough-p (>= n-frames 2))
-              ;;
-              (max-time (- (third
-                            (aref shot (1- n-frames)))
-                           .01))
-              ;;(now      (* (slot-value camera 'aspeed) (mynow))) ;; FIXME
-              ;;(time     (mod now max-time))
-              (animp (< (slot-value camera 'atime) max-time))
-              (time  (min max-time
-                          (incf (slot-value camera 'atime)
-                                (* (slot-value camera 'aspeed)
-                                   .01))))
-              ;;
-              (ipos     (1- (position-if
-                             (lambda (x) (< time (third x)))
-                             shot)))
-              (index    (if ipos
-                            ipos
-                            0))
-              ;;
-              (c-time   (third (aref shot index)))
-              (n-time   (third (aref shot (1+ index))))
-              (lerp     (/ (-   time c-time)
-                           (- n-time c-time))))
-    (setf (rot camera)
-          (q:lerp (second (aref shot index))
-                  (second (aref shot (1+ index)))
-                  lerp))
-    (setf (pos camera)
-          (v3:lerp (first (aref shot index))
-                   (first (aref shot (1+ index)))
-                   lerp))))
+  (defun switcharoo (camera)
+    "REPL helper, switch to camera...and switch back"
+    (unless (equal camera *currentcamera*)
+      (let ((current-pos (copy-seq (pos *currentcamera*)))
+            (current-rot (copy-seq (rot *currentcamera*))))
+        (if (v3:0p old-pos)
+            (setf old-pos current-pos
+                  old-rot current-rot
+                  (pos camera) (v3! 0))
+            (setf (pos camera) old-pos
+                  (rot camera) old-rot
+                  old-pos (v3! 0)))))))
 
 ;;--------------------------------------------------
 ;; UPDATE
 ;;--------------------------------------------------
 (defgeneric update (camera dt))
 (defmethod update ((camera orth) dt))
-(defmethod update ((camera pers) dt)
-  ;; (with-slots (pos) camera
-  ;;   ;; no backwards
-  ;;   (setf (z pos) (min (z pos) 50f0))
-  ;;   (setf (x pos) (alexandria:clamp (x pos) -50f0 50f0)))
-  ;;--------------------------------------------------
-  ;;(animate camera)
-  ;;--------------------------------------------------
-  )
+(defmethod update ((camera pers) dt))
 
