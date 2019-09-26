298eca405def1e3882c7929ae078783c93fa3019
azimut <azimut.github@protonmail.com>
2019-09-26 16:55:12 -0300

    godrays weight as parameter

--- godrays.lisp
+++ godrays.lisp
@@ -5,68 +5,78 @@
 ;; - *light-pos* v3 light-position
 ;; - sam1 rgb16f? sampler with only the bright light
 ;; dimensions-v2
+
+;; NT:
+;; This gets 2 effects
+;; 1) an halo around the point given
+;; 2) some other rayshafts around objects on screen
+;;
+;; * won't work with spotlight?
+;; * shafts can exist without the first one (i.e. object out of screen),
+;;   but can look really fake without it or when there is no object across which cast shafts
+;; * TLDR this effect requires tweaks between shots to look good, like adding
+;;   a multipler in the post process.
 (defvar *god-fbo* NIL)
 (defvar *god-sam* NIL)
-(defvar *dimensions-v2* NIL)
 
 (defun free-god ()
   (when *god-fbo* (free *god-fbo*)))
 
 (defun init-god ()
   (free-god)
-  (setf *dimensions-v2* (v! *dimensions*))
-  (setf *god-fbo* (make-fbo `(0 :element-type :rgb16f :dimensions ,*dimensions*)))
-  (setf *god-sam* (sample (attachment-tex *god-fbo* 0) :wrap :clamp-to-edge)))
+  (setf *god-fbo* (make-fbo `(0 :element-type :rgb16f :dimensions ,*dimensions*))
+        *god-sam* (sample (attachment-tex *god-fbo* 0) :wrap :clamp-to-edge)))
 
-(defun draw-god (sam1 time)
-  (declare (type single-float time))
+(defun draw-god (sam1 time &key (weight .1)
+                                (pos *light-pos*))
+  (declare (type cepl:sampler sam1)
+           (type single-float time))
   (with-fbo-bound (*god-fbo*)
-    (map-g #'god-rays-pipe *bs*
-           :res *dimensions-v2*
-           :time time
-           :sam sam1
-           :sun-pos (screen-coord *dimensions-v2*
-                                  *light-pos*))))
+    (with-setf (clear-color) (v! 0 0 0 0)
+      (map-g #'god-rays-pipe *bs*
+             :res (viewport-resolution (current-viewport))
+             :time time
+             :weight weight
+             :sam sam1
+             :sun-pos (screen-coord (viewport-resolution (current-viewport))
+                                    pos)))))
 
 (defun-g fbm-hash ((p :vec2))
   (fract
    (* (sin (dot p (v! 41 289)))
       "45758.5453")))
 
-(defun-g god-rays-frag ((uv :vec2)
+(defun-g god-rays-frag ((uv      :vec2)
                         &uniform
-                        (res :vec2)
-                        (time :float)
+                        (weight  :float)
                         (sun-pos :vec2)
-                        (sam :sampler-2d))
+                        (time    :float)
+                        (res     :vec2)
+                        (sam     :sampler-2d))
   (let* (;;(uv (/ (s~ gl-frag-coord :xy) res))
          ;; (v2:+s (v2:*s (v2:/ (screen-coord (resolution (current-viewport)) (v! 0 0 -30))
          ;;                     (resolution (current-viewport)))
          ;;               2f0)
          ;;   -1f0)
          (uv         uv)
-         (samples    10)
-         (decay      .974)
-         (exposure   .24)
-         (density    .93)
-         (weight     .36)
-         (color      (s~ (texture sam uv)
-                         :xyz))
-         (occ        (x color))
-         (obj        (y color))
+         (samples    20) ; 20
+         (decay      .9) ; .9 ;; the ghost outside the halo
+         (exposure   .4) ; .4 ;; the halo
+         ;;(density    .2)
+         ;;(weight     .8) ;.8
+         (illumdecay .3) ;.1 ;; intensity
+         (color      (texture sam uv))
+         (occ        (y color))
          (dtc        (* (- uv sun-pos)
                         (/ 1f0 samples)))
-         (illumdecay .4f0)
          (dither     (fbm-hash (+ uv (fract time)))))
     (dotimes (i samples)
       (decf uv dtc)
-      (let ((s (x (texture sam (+ (* dither dtc)
-                                  uv)))))
+      (let ((s (x (texture sam (+ uv (* dither dtc))))))
         (multf s (* illumdecay weight))
         (incf  occ s)
         (multf illumdecay decay)))
-    (v! (+ (v! 0 0 0)
-           (* occ exposure))
+    (v! (vec3 (* occ exposure))
         1)))
 
 (defpipeline-g god-rays-pipe (:points)
