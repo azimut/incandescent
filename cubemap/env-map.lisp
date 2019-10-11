e91fb68371ecab4a421275f375bb89116cf1c949
azimut <azimut.github@protonmail.com>
2019-10-10 23:23:55 -0300

    cubemap: envmap color fudge, new shadow/voxel params

--- cubemap/env-map.lisp
+++ cubemap/env-map.lisp
@@ -3,22 +3,28 @@
 (defclass env-map (actor)
   ((cubetex :initarg :cubetex)
    (cubesam :initarg :cubesam)
-   (buf     :initform (box))))
+   (buf     :initform (box)))
+  (:default-initargs
+   :voxelize-p nil
+   :shadow-p nil))
 
-(defun make-env-map (tex sam)
+(defun make-env-map (tex sam &optional (color (v! 1 1 1)))
   (declare (type cepl:texture tex)
            (type cepl:sampler sam))
-  (let ((obj (make-instance 'env-map :cubetex tex :cubesam sam :draw-p nil)))
+  (let ((obj (make-instance 'env-map
+                            :color color
+                            :cubetex tex :cubesam sam)))
     (push obj *actors*)
     obj))
 
 (defmethod draw ((actor env-map) camera time)
-  (with-slots (buf cubesam) actor
+  (with-slots (buf cubesam color) actor
     (with-setf* ((cull-face) :front
                  (depth-test-function) #'<=
                  (depth-mask) nil)
       (map-g #'cubemap-pipe buf
              :tex cubesam
+             :color color
              ;; Rotation without translation
              :view (q:to-mat4
                     (q:inverse (rot camera)))
