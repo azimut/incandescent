0c6c84ee2e7676de2371c4ef4a5e57faeaeed4ea
azimut <azimut.github@protonmail.com>
2019-10-17 14:53:18 -0300

    text: simplify screen render

--- text/screen.lisp
+++ text/screen.lisp
@@ -1,62 +1,57 @@
-(in-package :incandescent)
+(in-package #:incandescent)
+
+;; 1 Text
+;;
+;; NOTE: no idea how to hook it nicely into the main
+;; program, as putting in *actors* would made it
+;; pass trough a postprocessing tonemapping or worst.
+;; So I just Add the function screen-text after map-g for post-processing is done.
+
+(defvar *the-text* nil)
 
 (defclass text ()
-  ((pos  :initarg :pos  :accessor pos :documentation "2D screen position")
-   (text :initarg :text :accessor text))
+  ((pos   :initarg :pos   :accessor pos   :documentation "2D screen position")
+   (text  :initarg :text  :accessor text)
+   (color :initarg :color :accessor color)
+   (scale :initarg :scale :accessor scale))
   (:default-initargs
-   :pos (v! 0 0)))
+   :scale 1f0
+   :pos (v! 0 0)
+   :color (v! 1 1 1)))
 
 ;;--------------------------------------------------
 
-(defun init-text (&key (charset *default-charset*)
-                       (font    *default-font*)
-                       force)
-  (declare (type string   charset)
-           (type pathname font))
-  (when (or force (not *font*))
-    (setf *font* (cepl.fond:make-fond-font font charset))
-    (setf *text* (cepl.fond:make-fond-text *font* "...")))
-  t)
-
-;;--------------------------------------------------
-
-(defun kill-text ()
-  (alexandria:removef *actors* (find-actor-class 'text))
-  t)
-
-(defun make-text (&optional (string "hello!")
-                            (pos (v! 0 0)))
+(defun make-text (string &key (pos (v! 0 0))
+                              (scale 1f0)
+                              (color (v! 1 1 1)))
   "opinionated constructor, mainly to be used for a single
    text overlay in front of all things"
   (declare (type (or null string) string)
-           (type rtg-math.types:vec2 pos))
-  (if (null string)
-      (kill-text)
-      (let ((current (find-actor-class 'text)))
-        (if current
-            (progn
-              (setf (text current) string)
-              (setf (pos current)  pos)
-              (cepl.fond:update-fond-text *text* string)
-              current)
-            (let ((obj (make-instance 'text :text string :pos pos)))
-              (cepl.fond:update-fond-text *text* string)
-              (push obj *actors*)
-              obj)))))
+           (type rtg-math.types:vec2 pos)
+           (type rtg-math.types:vec3 color))
+  (if *the-text*
+      (setf (text  *the-text*) string
+            (pos   *the-text*) pos
+            (color *the-text*) color
+            (scale *the-text*) scale)
+      (setf *the-text*
+            (make-instance 'text :text string
+                                 :pos pos
+                                 :scale scale
+                                 :color color)))
+  (cepl.fond:update-fond-text *text* string)
+  t)
 
 ;;--------------------------------------------------
 
-(defmethod update ((actor text) dt))
-
-(defmethod draw ((actor text) camera time)
-  (with-slots (pos) actor
+(defun screen-text ()
+  (with-slots (pos color scale) *the-text*
     (with-blending *font-blending*
-      (with-setf* (;;(depth-mask) nil
-                   ;;(depth-test-function) nil
-                   (cull-face) nil
-                   ;;(depth-test-function) #'never
-                   )
-        (map-g #'fondness  (cepl.fond::fond-text-stream *text*)
-               :extent     (v! pos *dimensions*)
-               :tex-image  (cepl.fond::fond-font-sampler *font*)
-               :text-color (v! 1 1 1 1))))))
+      (with-setf* ((depth-mask) nil
+                   (depth-test-function) nil
+                   (cull-face) nil)
+        (map-g #'fondness (cepl.fond::fond-text-stream *text*)
+               :extent (v! pos *dimensions*)
+               :scale scale
+               :tex-image (cepl.fond::fond-font-sampler *font*)
+               :text-color color)))))
--- text/text.lisp
+++ text/text.lisp
@@ -1,16 +1,15 @@
-(in-package :incandescent)
+(in-package #:incandescent)
 
 ;; Everything on cepl.fond/cl-fond goes around updating the buffer stream
 ;; with every new text update.
 ;; See: (cepl.fond:update-fond-text *text* string)
 ;;
-;; -Might be possible to re-use the same texture but not by default.-
+;; - Might be possible to re-use the same texture but not by default.-
+;;
 ;; See: (setf *text* (cepl.fond:make-fond-text *font* "a text"))
 
-(defvar *default-font*    (resolve-path "static/Courier New.ttf"))
-(defparameter *default-charset* "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .!?()0123456789-")
-
-;; 2D On screen text helpers
+(defvar *default-font* (truename "static/Courier New.ttf"))
+(defvar *default-charset* "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ .!?()0123456789-")
 
 (defvar *font* nil "cepl.fond:fond-font struct
                     - font
@@ -23,3 +22,16 @@
 
 (defparameter *font-blending*
   (make-blending-params :source-alpha :one-minus-src-alpha))
+
+;;--------------------------------------------------
+
+(defun init-text (&key (charset *default-charset*)
+                       (font    *default-font*)
+                       force)
+  "initializes global CEPL.FOND"
+  (declare (type string   charset)
+           (type pathname font))
+  (when (or force (not *font*))
+    (setf *font* (cepl.fond:make-fond-font font charset))
+    (setf *text* (cepl.fond:make-fond-text *font* "...")))
+  t)
