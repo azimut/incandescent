6a979f732f4c8202439845448c49699228564798
azimut <azimut.github@protonmail.com>
2020-06-04 22:27:39 -0300

    sample light colors lists

--- draw.lisp
+++ draw.lisp
@@ -7,11 +7,6 @@
 
 (defparameter *parallax-scale* .01f0)
 
-(defparameter *light-color* (v! 0.78823537 0.8862746 1.0))
-(defparameter *light-color* (v! 1.0 0.8196079 0.69803923))
-(defparameter *light-color* (v! 0.2509804 0.6117647 1.0))
-(defparameter *light-color* (v! 1.0 0.9568628 0.8980393))
-
 ;; http://planetpixelemporium.com/tutorialpages/light.html
 ;; Candle           1900 255  147   41   (v! 1.0 0.5764706 0.16078432)
 ;; 40W Tungsten     2600 255  197  143   (v! 1.0 0.7725491 0.56078434)
@@ -22,7 +17,6 @@
 ;; Direct Sunlight  6000 255  255  255   (v! 1.0 1.0 1.0)
 ;; Overcast Sky     7000 201  226  255   (v! 0.78823537 0.8862746 1.0)
 ;; Clear Blue Sky  20000  64  156  255   (v! 0.2509804 0.6117647 1.0)
-;;
 ;; Warm Fluorescent          255 244 229 (v! 1.0 0.9568628 0.8980393)
 ;; Standard Fluorescent      244 255 250 (v! 0.9568628 1.0 0.9803922)
 ;; Cool White Fluorescent    212 235 255 (v! 0.8313726 0.9215687 1.0)
@@ -33,6 +27,29 @@
 ;; Sodium Vapor              255 209 178 (v! 1.0 0.8196079 0.69803923)
 ;; Metal Halide              242 252 255 (v! 0.9490197 0.98823535 1.0)
 ;; High Pressure Sodium      255 183 76  (v! 1.0 0.7176471 0.29803923)
+(defvar *sample-colors*
+  (list (v! 1.0        0.5764706  0.16078432)
+        (v! 1.0        0.7725491  0.56078434)
+        (v! 1.0        0.83921576 0.6666667)
+        (v! 1.0        0.9450981  0.87843144)
+        (v! 1.0        0.9803922  0.9568628)
+        (v! 1.0        1.0        0.9843138)
+        (v! 1.0        1.0        1.0)
+        (v! 0.78823537 0.8862746  1.0)
+        (v! 0.2509804  0.6117647  1.0)
+        (v! 1.0        0.9568628  0.8980393)
+        (v! 0.9568628  1.0        0.9803922)
+        (v! 0.8313726  0.9215687  1.0)
+        (v! 1.0        0.9568628  0.9490197)
+        (v! 1.0        0.93725497 0.9686275)
+        (v! 0.654902   0.0        1.0)
+        (v! 0.8470589  0.9686275  1.0)
+        (v! 1.0        0.8196079  0.69803923)
+        (v! 0.9490197  0.98823535 1.0)
+        (v! 1.0        0.7176471  0.29803923))
+  "Length=19")
+
+(defparameter *light-color* (nth 2 *sample-colors*))
 
 (defun render-all-the-things (actor camera time)
   (declare (single-float time))
